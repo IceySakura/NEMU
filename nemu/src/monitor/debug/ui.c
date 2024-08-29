855fc33f2dc39c4be85e4b0a97a995953d2c27fd
tracer-NEMU2020 <tjuics2020@163.com>
2024-08-29 20:03:00 +0800

    >  compile NEMU 3022232100 iceysakura Linux Legion-Singer 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  20:03:00 up  4:16,  1 user,  load average: 0.14, 0.12, 0.04 6eafc256f840f4505de622903d453efc513afa96

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -57,7 +57,7 @@ static struct {
 	{ "help", "Display informations about all supported commands", cmd_help },
 	{ "c", "Continue the execution of the program", cmd_c },
 	{ "q", "Exit NEMU", cmd_q },
-	{ "si [N]", "Execute for N steps, N default for 1", cmd_si},
+	{ "si", "si [N], Execute for N steps, N default for 1", cmd_si},
 
 	/* TODO: Add more commands */
 
