a0998e5c3cd0d9e3d7be438ca4cafaff816dedbc
tracer-NEMU2020 <tjuics2020@163.com>
2024-09-17 00:57:47 +0800

    >  compile NEMU 3022232100 iceynew Linux Legion-Singer 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  00:57:47 up 12:46,  1 user,  load average: 0.04, 0.03, 0.00 bb944723aa98bd6a3d0d575fdf1fbb6d0bc826b

--- nemu/src/cpu/exec/control/jcc.c
+++ nemu/src/cpu/exec/control/jcc.c
@@ -3,3 +3,13 @@
 #define DATA_BYTE 1
 #include "jcc-template.h"
 #undef DATA_BYTE
+
+#define DATA_BYTE 2
+#include "jcc-template.h"
+#undef DATA_BYTE
+
+#define DATA_BYTE 4
+#include "jcc-template.h"
+#undef DATA_BYTE
+
+make_helper_v(jbe_i)
