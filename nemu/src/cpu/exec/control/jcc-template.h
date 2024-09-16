09f9b484703ef31d4d7a6f8bc6ee72cbe1986ef7
tracer-NEMU2020 <tjuics2020@163.com>
2024-09-17 00:53:51 +0800

    >  compile NEMU 3022232100 iceynew Linux Legion-Singer 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  00:53:51 up 12:42,  1 user,  load average: 0.02, 0.03, 0.00 d26c3725da829aeb7c116fac559621687ca0d1

--- nemu/src/cpu/exec/control/jcc-template.h
+++ nemu/src/cpu/exec/control/jcc-template.h
@@ -30,4 +30,24 @@ static void do_execute() {
 make_instr_helper(i)
 #undef instr
 
+#define instr jle
+static void do_execute() {
+	DATA_TYPE_S displacement = op_src->val;
+	if (cpu.eflags.ZF == 1 || cpu.eflags.SF != cpu.eflags.OF)
+		cpu.eip += displacement;
+	print_asm_template1();
+}
+make_instr_helper(i)
+#undef instr
+
+#define instr jl
+static void do_execute() {
+	DATA_TYPE_S displacement = op_src->val;
+	if (cpu.eflags.SF != cpu.eflags.OF)
+		cpu.eip += displacement;
+	print_asm_template1();
+}
+make_instr_helper(i)
+#undef instr
+
 #include "cpu/exec/template-end.h"
--- nemu/src/cpu/exec/control/jcc.h
+++ nemu/src/cpu/exec/control/jcc.h
@@ -6,4 +6,8 @@ make_helper(je_i_b);
 make_helper(jbe_i_b);
 make_helper(jb_i_b);
 
+make_helper(jle_i_b);
+make_helper(jl_i_b);
+
+
 #endif
