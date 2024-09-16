da25b7610d4334316f91f26d71b9f0803475c0eb
tracer-NEMU2020 <tjuics2020@163.com>
2024-09-16 22:49:28 +0800

    >  compile NEMU 3022232100 iceynew Linux Legion-Singer 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  22:49:28 up 10:38,  1 user,  load average: 0.02, 0.02, 0.00 8cf9a3abd31946bc68879c6ec39edc79b42af7c5

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -78,6 +78,10 @@ static int cmd_info(char *args)
 		printf("ebp\t\t0x%08x\t\t%d\n", cpu.ebp, cpu.ebp);
 		printf("esp\t\t0x%08x\t\t%d\n", cpu.esp, cpu.esp);
 		printf("eip\t\t0x%08x\t\t%d\n", cpu.eip, cpu.eip);
+
+		// Log eflags
+		Log("CF\tPF\tAF\tZF\tSF\tTF\tIF\tDF\tOF\n");
+		Log("%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\n", cpu.eflags.CF, cpu.eflags.PF, cpu.eflags.AF, cpu.eflags.ZF, cpu.eflags.SF, cpu.eflags.TF, cpu.eflags.IF, cpu.eflags.DF, cpu.eflags.OF);
 	}
 	else if(args[0] == 'w')
 	{
