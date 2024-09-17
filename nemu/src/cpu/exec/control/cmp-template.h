97053dfcc4a66864652f90784bdd9970ae6c9489
tracer-NEMU2020 <tjuics2020@163.com>
2024-09-17 11:57:44 +0800

    >  compile NEMU 3022232100 iceynew Linux Legion-Singer 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  11:57:44 up 59 min,  1 user,  load average: 0.11, 0.06, 0.01 d339b5d2d7ad9e354a752838d2ed5ef32c04d688

--- nemu/src/cpu/exec/control/cmp-template.h
+++ nemu/src/cpu/exec/control/cmp-template.h
@@ -8,14 +8,16 @@ static void do_execute() {
 	DATA_TYPE_S result = dest - src;
 	OPERAND_W(op_dest, result);
 	// update EFLAGS
-	update_eflags_pf_zf_sf(result);
+	update_eflags_pf_zf_sf((int32_t)result);
 	cpu.eflags.CF = dest < src;
 	cpu.eflags.OF = MSB(dest) != MSB(src) && MSB(dest) != MSB(result);
 	print_asm_template2();
 }
 
 make_instr_helper(rm_imm)
+make_instr_helper(i2a)
 make_instr_helper(i2r)
+make_instr_helper(i2rm)
 make_instr_helper(rm2r)
 make_instr_helper(r2rm)
 
--- nemu/src/cpu/exec/control/cmp.h
+++ nemu/src/cpu/exec/control/cmp.h
@@ -3,8 +3,14 @@
 
 make_helper(cmp_rm_imm_v);
 
+make_helper(cmp_i2a_b);
+make_helper(cmp_i2a_v);
+
 make_helper(cmp_i2r_b);
 make_helper(cmp_i2r_v);
+
+make_helper(cmp_i2rm_b);
+make_helper(cmp_i2rm_v);
 
 make_helper(cmp_rm2r_b);
 make_helper(cmp_rm2r_v);
