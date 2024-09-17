4cf58ec96f28a3df61c9ea63105399d6452da9e3
tracer-NEMU2020 <tjuics2020@163.com>
2024-09-17 12:21:30 +0800

    >  compile NEMU 3022232100 iceynew Linux Legion-Singer 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  12:21:30 up  1:23,  1 user,  load average: 0.00, 0.01, 0.00 c8dcbada65eaeb12713d5f3c23fe2b304f864f86

--- nemu/src/cpu/exec/control/cmp-template.h
+++ nemu/src/cpu/exec/control/cmp-template.h
@@ -6,7 +6,6 @@ static void do_execute() {
 	DATA_TYPE_S src = op_src->val;
 	DATA_TYPE_S dest = op_dest->val;
 	DATA_TYPE_S result = dest - src;
-	OPERAND_W(op_dest, result);
 	// update EFLAGS
 	update_eflags_pf_zf_sf((int32_t)result);
 	cpu.eflags.CF = dest < src;
