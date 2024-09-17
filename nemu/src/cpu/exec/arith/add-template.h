9863154143f126c0b33295c6209515688a21a9fb
tracer-NEMU2020 <tjuics2020@163.com>
2024-09-17 12:31:21 +0800

    >  compile NEMU 3022232100 iceynew Linux Legion-Singer 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  12:31:21 up  1:33,  1 user,  load average: 0.00, 0.02, 0.00 12b67571d4ee7638bc368fc85e15029a7c18821

--- nemu/src/cpu/exec/arith/add-template.h
+++ nemu/src/cpu/exec/arith/add-template.h
@@ -3,10 +3,11 @@
 #define instr add
 
 static void do_execute () {
-	DATA_TYPE result = op_dest->val + (int8_t)op_src->val;
+	DATA_TYPE result = op_dest->val + op_src->val;
 	OPERAND_W(op_dest, result);
 }
 
 make_instr_helper(rm_imm)
+make_instr_helper(i2a)
 
 #include "cpu/exec/template-end.h"
--- nemu/src/cpu/exec/arith/add.h
+++ nemu/src/cpu/exec/arith/add.h
@@ -2,5 +2,7 @@
 #define __ADD_H__
 
 make_helper(add_rm_imm_v);
+make_helper(add_i2a_b);
+make_helper(add_i2a_v);
 
 #endif
