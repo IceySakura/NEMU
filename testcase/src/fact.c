92ae9948eeefe257a9752b27cb8bdc65e17121f6
tracer-NEMU2020 <tjuics2020@163.com>
2024-09-17 12:08:38 +0800

    >  run 3022232100 iceynew Linux Legion-Singer 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  12:08:38 up  1:10,  1 user,  load average: 0.02, 0.06, 0.01 c573ead6d8fba2996605720d5543c7fc36bc3c8a

--- testcase/src/fact.c
+++ testcase/src/fact.c
@@ -10,13 +10,12 @@ int fact(int n) {
 
 int main() {
 	int i;
-	int num = 13;
-	for(i = 0; i < num; i ++) {
+	for(i = 0; i < 13; i ++) {
 		f[i] = fact(i);
 		nemu_assert(f[i] == ans[i]);
 	}
 
-	nemu_assert(i == num);
+	nemu_assert(i == 13);
 
 	return 0;
 }
