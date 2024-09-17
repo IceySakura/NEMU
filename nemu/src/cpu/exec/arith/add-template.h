74ff1ae56e542dea55c5d0cbd4dad00885f765b7
tracer-NEMU2020 <tjuics2020@163.com>
2024-09-17 12:51:42 +0800

    >  compile NEMU 3022232100 iceynew Linux Legion-Singer 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  12:51:42 up  1:53,  1 user,  load average: 0.06, 0.03, 0.00 ec5d7c2e3a634bb87fdb5951c1b8695263df0f0

--- nemu/src/cpu/exec/arith/add-template.h
+++ nemu/src/cpu/exec/arith/add-template.h
@@ -3,7 +3,13 @@
 #define instr add
 
 static void do_execute () {
-	DATA_TYPE result = op_dest->val + op_src->val;
+	DATA_TYPE_S src;
+	if(op_src->size != op_dest->size) 
+		src = (int8_t)op_src->val; // sign extension
+	else
+		src = op_src->val;
+	DATA_TYPE_S dest = op_dest->val;
+	DATA_TYPE_S result = dest + src;
 	OPERAND_W(op_dest, result);
 }
 
